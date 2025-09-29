984c6e99e14bb3f480c1b38e374dfab2d392eb16
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-09-29 14:09:26 +0200

    feat: email & nickname made unique (@poiz0002's feedback)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,12 +6,14 @@ use App\Enum\Gender;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_NICKNAME', fields: ['nickname'])]
+#[UniqueEntity(fields: ['email', 'nickname'])]
+#[ORM\UniqueConstraint(name: 'UNIQUE_IDENTIFIERS', fields: ['nickname', 'email'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
