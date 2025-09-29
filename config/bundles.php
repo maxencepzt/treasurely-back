400ce6376153f2ba986fdf61fcc023d7019cf615
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-09-29 14:14:38 +0200

    build: doctrine extensions installed (@poiz0002's feedback)

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
