7fe2d5ac8b073fe6694d08bfc9bae4d950b9661a
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-09-30 15:02:22 +0200

    feat: entity Riddle created

---
+++ src/Entity/Riddle.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RiddleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: RiddleRepository::class)]
+class Riddle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private string $title;
+
+    #[ORM\Column(length: 1000)]
+    private string $description;
+
+    #[ORM\Column]
+    #[Assert\Choice(
+        choices: [1, 2, 3],
+    )]
+    private int $difficulty;
+
+    #[ORM\Column]
+    private int $orderNumber;
+
+    #[ORM\ManyToOne(inversedBy: 'riddles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private TreasureHunt $hunt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDifficulty(): int
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(int $difficulty): static
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getOrderNumber(): int
+    {
+        return $this->orderNumber;
+    }
+
+    public function setOrderNumber(int $orderNumber): static
+    {
+        $this->orderNumber = $orderNumber;
+
+        return $this;
+    }
+
+    public function getHunt(): TreasureHunt
+    {
+        return $this->hunt;
+    }
+
+    public function setHunt(TreasureHunt $hunt): static
+    {
+        $this->hunt = $hunt;
+
+        return $this;
+    }
+}
