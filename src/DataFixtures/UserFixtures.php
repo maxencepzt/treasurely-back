361d630fc4f89d26a91bcb2e0ee9401589e4be03
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-09-30 11:47:27 +0200

    feat: added dependency

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,14 +5,22 @@ namespace App\DataFixtures;
 use App\Factory\PictureFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 10; ++$i) {
             UserFactory::createOne(['profilePicture' => PictureFactory::CreateOne()]);
         }
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            PictureFixtures::class,
+        ];
     }
 }
