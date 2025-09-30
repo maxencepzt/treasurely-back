fdfaf3f193aa027d47326462388e1b47c6a82b74
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-09-30 15:49:50 +0200

    feat: entity GPSRiddle created

---
+++ src/Entity/GPSRiddle.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GPSRiddleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GPSRiddleRepository::class)]
+class GPSRiddle
+{
+    #[ORM\Column]
+    private float $latitude;
+
+    #[ORM\Column]
+    private float $longitude;
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+}
