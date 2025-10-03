8306f27ba77c26d6546b39751ac43be21c0d2970
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-10-03 15:24:24 +0200

    feat: add user detail retrieval test case

---
+++ tests/Api/User/UserGetCest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Api\User;
+
+use App\Entity\User;
+use App\Enum\Gender;
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+
+final class UserGetCest
+{
+    /**
+     * @return array<string, string>
+     */
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'nickname' => 'string',
+            'firstname' => 'string',
+            'lastname' => 'string',
+            'email' => 'string:email',
+            'birthDate' => 'string:date',
+            'phone' => 'string',
+            'creationDate' => 'string:date',
+            'public' => 'boolean',
+            'gender' => 'string',
+            'profilePicture' => 'array',
+            'totalTime' => 'integer',
+            'totalHunt' => 'integer',
+        ];
+    }
+
+    public function getUserDetail(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        $birthDate = new \DateTime('1990-01-01');
+        $creationDate = new \DateTimeImmutable('2020-01-01 00:00:00', new \DateTimeZone('UTC'));
+
+        $user = UserFactory::createOne([
+            'nickname' => 'johndoe',
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'email' => 'johndoe@example.com',
+            'birthDate' => $birthDate,
+            'phone' => '1234567890',
+            'creationDate' => $creationDate,
+            'public' => true,
+            'gender' => Gender::MAN,
+            'totalTime' => 3600,
+            'totalHunt' => 15,
+            'password' => 'test',
+        ])->_real();
+
+        // 2. 'Act'
+        $I->amLoggedInAs($user);
+        $I->sendGet('/api/users/'.$user->getId());
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(User::class, '/api/users/'.$user->getId());
+
+        $expectedData = [
+            'nickname' => 'johndoe',
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'email' => 'johndoe@example.com',
+            'birthDate' => $birthDate->format(\DateTimeInterface::W3C),
+            'phone' => '1234567890',
+            'creationDate' => $creationDate->format(\DateTimeInterface::W3C),
+            'public' => true,
+            'gender' => Gender::MAN->value,
+            'totalTime' => 3600,
+            'totalHunt' => 15,
+        ];
+
+        $I->seeResponseIsAnItem(self::expectedProperties(), $expectedData);
+    }
+}
