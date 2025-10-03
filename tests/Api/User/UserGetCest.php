938f19305171f8f8feff3771b7cca4849d2cc7f2
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-10-03 16:03:29 +0200

    chore: move user test collection to UserGetCollectionCest.php

--- tests/Api/User/UserGetCest.php
+++ tests/Api/User/UserGetCest.php
@@ -142,30 +142,4 @@ final class UserGetCest
 
         $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
-
-    public function passwordIsNotReturnedInResponse(ApiTester $I): void
-    {
-        // Le mot de passe ne doit jamais être retourné
-        $user = UserFactory::createOne()->_real();
-
-        $I->amLoggedInAs($user);
-        $I->sendGet('/api/users/'.$user->getId());
-
-        $I->seeResponseCodeIsSuccessful();
-        $I->dontSeeResponseJsonMatchesJsonPath('$.password');
-    }
-
-    public function getCollectionDoesNotIncludeDeactivatedUsers(ApiTester $I): void
-    {
-        // La collection ne retourne que les utilisateurs activés
-        UserFactory::createOne(['activated' => true])->_real();
-        UserFactory::createOne(['activated' => false])->_real();
-        $authenticatedUser = UserFactory::createOne()->_real();
-
-        $I->amLoggedInAs($authenticatedUser);
-        $I->sendGet('/api/users');
-
-        $I->seeResponseCodeIsSuccessful();
-        $I->dontSeeResponseContainsJson(['activated' => false]);
-    }
 }
