434d975ef5c3623e78d0a6c9ea58d64cb1ae5cc5
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-10-20 13:50:36 +0200

    feat: implement JWT refresh token functionality

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
