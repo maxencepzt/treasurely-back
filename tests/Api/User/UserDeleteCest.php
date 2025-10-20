faa1bee32ee6b206ee30a1954e4665535d27efc0
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-10-20 15:35:28 +0200

    test: add canDeleteOwnAccount test

---
+++ tests/Api/User/UserDeleteCest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Api\User;
+
+use App\Entity\User;
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
+final class UserDeleteCest
+{
+    public function canDeleteOwnAccount(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        $user = UserFactory::createOne([
+            'nickname' => 'deleteme',
+            'password' => 'ValidPass123!',
+        ]);
+        $I->amLoggedInAs($user->_real());
+
+        // 2. 'Act'
+        $I->sendDelete('/api/users/'.$user->getId());
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIs(HttpCode::NO_CONTENT);
+        $I->dontSeeInRepository(User::class, ['nickname' => 'deleteme']);
+    }
+}
