91a46837917b10b0b01ac01bce713c3ded2933f4
Jules DESCOUTURES <jules.descoutures@etudiant.univ-reims.fr>
2025-10-21 16:57:24 +0200

    feat: participateHunt fixtures created, dependencies added

---
+++ src/DataFixtures/ParticipateHuntFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ParticipateHuntFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+
+    /**
+     * @return class-string<FixtureInterface>[]
+     */
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+            TreasureHuntFixtures::class,
+            RiddleFixtures::class,
+            // ParticipateRiddleFixtures::class,
+        ];
+    }
+}
