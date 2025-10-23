1524f83ded5ae0c30d79563090c18f4280d2d020
Jules DESCOUTURES <jules.descoutures@etudiant.univ-reims.fr>
2025-10-23 10:04:56 +0200

    build: add new method calculateScore

--- src/Service/ScoreCalculator.php
+++ src/Service/ScoreCalculator.php
@@ -6,4 +6,10 @@ namespace App\Service;
 
 class ScoreCalculator
 {
+    public function calculateScore(\DateTimeImmutable $startTime, \DateTimeImmutable $endTime, int $difficulty): int
+    {
+        $time = $endTime->getTimestamp() - $startTime->getTimestamp();
+
+        return (int) ($difficulty * (1000 * exp(-0.001 * (int) abs($time))));
+    }
 }
