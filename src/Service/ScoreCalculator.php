9019d6ab709c9bbe4fb288cc8bac004a513e7ce1
Jules DESCOUTURES <jules.descoutures@etudiant.univ-reims.fr>
2025-10-23 10:05:40 +0200

    build: add new method canUserParticipate

--- src/Service/ScoreCalculator.php
+++ src/Service/ScoreCalculator.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\Riddle;
+use App\Entity\User;
+
 class ScoreCalculator
 {
     public function calculateScore(\DateTimeImmutable $startTime, \DateTimeImmutable $endTime, int $difficulty): int
@@ -11,5 +14,27 @@ class ScoreCalculator
         $time = $endTime->getTimestamp() - $startTime->getTimestamp();
 
         return (int) ($difficulty * (1000 * exp(-0.001 * (int) abs($time))));
+    }
+
+    public function canUserParticipate(User $user, Riddle $riddle): bool
+    {
+        $hunt = $riddle->getHunt();
+        $check = true;
+
+        if ($user->getId() != $hunt->getOwner()->getId()) {
+            $team = $hunt->getTeam();
+            $members = $team->getMembers();
+            foreach ($members as $member) {
+                if ($user->getId() != $member->getId() && $check) {
+                    $check = true;
+                } else {
+                    $check = false;
+                }
+            }
+        } else {
+            $check = false;
+        }
+
+        return $check;
     }
 }
