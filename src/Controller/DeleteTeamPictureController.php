8ecba0aa1cd27829269ad98bb5de76367a29ff0c
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-10-24 16:22:34 +0200

    feat: add controller to delete picture for team

---
+++ src/Controller/DeleteTeamPictureController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Team;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class DeleteTeamPictureController extends AbstractController
+{
+    public function __invoke(Team $data, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $roles = $user->getRoles();
+        if (in_array('ROLE_ADMIN', $roles) || $data->getOwner() === $user) {
+            $image = $data->getImage();
+            $em->remove($image);
+            $data->setImage(null);
+            $em->persist($data);
+            $em->flush();
+
+            return new Response(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return new Response(null, Response::HTTP_FORBIDDEN);
+    }
+}
