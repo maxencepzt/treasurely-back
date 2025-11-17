b549f1769bc08c06ab70c5350c4f6c2e56da09e5
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-17 15:16:06 +0100

    feat: implement __toString() method
    # Conflicts:
    #	src/Entity/User.php

--- src/Entity/HuntType.php
+++ src/Entity/HuntType.php
@@ -92,4 +92,9 @@ class HuntType
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->title;
+    }
 }
--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -69,4 +69,9 @@ class Picture
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return sprintf('Image #%d', $this->id ?? 0);
+    }
 }
