b33d907d623598b28bcf4146a7fe8c5b5be435a2
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-19 16:56:49 +0100

    feat: real team display
    # Conflicts:
    #	src/Repository/TeamRepository.php

--- src/Repository/TeamRepository.php
+++ src/Repository/TeamRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Team;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +15,33 @@ class TeamRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Team::class);
+    }
+
+    /**
+     * @return Team[] Returns an array of Team objects owned by the user
+     */
+    public function findByOwner(User $owner): array
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.owner = :owner')
+            ->setParameter('owner', $owner)
+            ->orderBy('t.id', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @return Team[] Returns an array of Team objects where user is a member
+     */
+    public function findByMember(User $user): array
+    {
+        return $this->createQueryBuilder('t')
+            ->innerJoin('t.members', 'm')
+            ->andWhere('m = :user')
+            ->andWhere('t.owner != :user')
+            ->setParameter('user', $user)
+            ->orderBy('t.id', 'DESC')
+            ->getQuery()
+            ->getResult();
     }
 }
