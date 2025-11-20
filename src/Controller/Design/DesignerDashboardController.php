04703767b046ffdaf7e10d8cbf17cbb8337d15cb
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-20 11:01:44 +0100

    feat: add statistics in designer dashboard

--- src/Controller/Design/DesignerDashboardController.php
+++ src/Controller/Design/DesignerDashboardController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller\Design;
 
+use App\Entity\User;
+use App\Repository\RiddleRepository;
+use App\Repository\TeamRepository;
+use App\Repository\TreasureHuntRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DesignerDashboardController extends AbstractController
 {
     #[Route('/designer/dashboard', name: 'app_designer_dashboard')]
-    public function index(): Response
-    {
+    public function index(
+        TeamRepository $teamRepository,
+        TreasureHuntRepository $treasureHuntRepository,
+        RiddleRepository $riddleRepository,
+        Security $security,
+    ): Response {
+        /**
+         * @var User $user
+         */
+        $user = $security->getUser();
+
+        // Calculer les statistiques
+        $nbTeams = $teamRepository->countByOwner($user);
+        $nbMembers = $teamRepository->countMembersByOwner($user);
+        $nbTreasureHunts = $treasureHuntRepository->countByTeamOwner($user);
+        $nbRiddles = $riddleRepository->countByTeamOwner($user);
+
         return $this->render('designer/dashboard/index.html.twig', [
-            'controller_name' => 'DesignerDashboardController',
+            'nb_teams' => $nbTeams,
+            'nb_members' => $nbMembers,
+            'nb_treasure_hunts' => $nbTreasureHunts,
+            'nb_riddles' => $nbRiddles,
         ]);
     }
 }
