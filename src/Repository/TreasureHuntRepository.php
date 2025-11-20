68a121993378f22f7d0224714b09ea3f27a75ee0
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-20 10:56:29 +0100

    feat: method to count treasure hunts by team owner

--- src/Repository/TreasureHuntRepository.php
+++ src/Repository/TreasureHuntRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\TreasureHunt;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +15,19 @@ class TreasureHuntRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, TreasureHunt::class);
+    }
+
+    /**
+     * Count treasure hunts in teams owned by the user.
+     */
+    public function countByTeamOwner(User $owner): int
+    {
+        return $this->createQueryBuilder('th')
+            ->select('COUNT(th.id)')
+            ->innerJoin('th.team', 't')
+            ->andWhere('t.owner = :owner')
+            ->setParameter('owner', $owner)
+            ->getQuery()
+            ->getSingleScalarResult();
     }
 }
