5b6de31854adcf36c9383012e39b9ea32628f6e4
Ylan Nicolas <ylan.nicolas@etudiant.univ-reims.fr>
2025-11-21 11:50:17 +0100

    Revert "fix: update team routes for treasure hunts"
    This reverts commit bf26f54b5d49b0082437978cb72d81c27b030573.

--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -91,16 +91,16 @@ use Symfony\Component\Serializer\Attribute\Groups;
             security: "is_granted('ROLE_USER')",
         ),
         new Get(
-            uriTemplate: 'teams/{id}/hunts',
+            uriTemplate: 'teams/{id}/treasure_hunts/designer',
             openapi: new Operation(
-                summary: 'Designer Teams treasure hunts',
-                description: 'Retrieve detailed informations of the treasure hunts designed from a specific team by their ID. Requires ROLE_USER permission.'
+                summary: 'Teams treasure hunts',
+                description: 'Retrieve detailed informations of the treasure hunts from a specific team by their ID. Requires ROLE_USER permission.'
             ),
             normalizationContext: ['groups' => ['team:treasureHunts']],
             security: "is_granted('ROLE_USER')",
         ),
         new Get(
-            uriTemplate: 'teams/{id}/participations',
+            uriTemplate: 'teams/{id}/treasure_hunts/player',
             openapi: new Operation(
                 summary: 'Player Teams treasure hunts participations',
                 description: 'Retrieve detailed informations of the treasure hunts played from a specific team by their ID. Requires ROLE_USER permission.'
