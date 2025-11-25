ed06c8cc8e33d0a0914fd7bbabb07c87e41df1ed
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-25 11:14:42 +0100

    feat: add serialization groups to TextRiddle

--- src/Entity/TextRiddle.php
+++ src/Entity/TextRiddle.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\TextRiddleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: TextRiddleRepository::class)]
 class TextRiddle extends Riddle
@@ -11,6 +12,7 @@ class TextRiddle extends Riddle
     #[ORM\Column(length: 100)]
     private string $answer;
 
+    #[Groups(['riddle:read'])]
     public function getAnswer(): string
     {
         return $this->answer;
