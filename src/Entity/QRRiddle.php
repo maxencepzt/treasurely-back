0d1690462ae6ed90eb42bd7ffaebc7fbb6430333
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-25 11:14:35 +0100

    feat: add serialization groups to QRRiddle

--- src/Entity/QRRiddle.php
+++ src/Entity/QRRiddle.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\QRRiddleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: QRRiddleRepository::class)]
 class QRRiddle extends Riddle
@@ -11,6 +12,7 @@ class QRRiddle extends Riddle
     #[ORM\Column(length: 20)]
     private string $code;
 
+    #[Groups(['riddle:read'])]
     public function getCode(): ?string
     {
         return $this->code;
