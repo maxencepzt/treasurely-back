87617acd395320be0e32fd51af4735a4df7a8784
Maxence Poizat <maxence.poizat@etudiant.univ-reims.fr>
2025-11-25 11:14:13 +0100

    feat: add serialization groups to GPSRiddle

--- src/Entity/GPSRiddle.php
+++ src/Entity/GPSRiddle.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\GPSRiddleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: GPSRiddleRepository::class)]
 class GPSRiddle extends Riddle
@@ -14,6 +15,7 @@ class GPSRiddle extends Riddle
     #[ORM\Column]
     private float $longitude;
 
+    #[Groups(['riddle:read'])]
     public function getLatitude(): ?float
     {
         return $this->latitude;
@@ -26,6 +28,7 @@ class GPSRiddle extends Riddle
         return $this;
     }
 
+    #[Groups(['riddle:read'])]
     public function getLongitude(): ?float
     {
         return $this->longitude;
