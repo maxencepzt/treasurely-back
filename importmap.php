2d4c0cf0104e12e4925937dc14b9587a8fa0506f
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-11-26 15:56:19 +0100

    feat: add riddle management

--- importmap.php
+++ importmap.php
@@ -40,6 +40,10 @@ return [
         'path' => './assets/js/file-drag-drop.js',
         'entrypoint' => true,
     ],
+    'riddle-manager' => [
+        'path' => './assets/js/riddle-manager.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
